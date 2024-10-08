3fc08e5a0a6d2d156ad45a3b2b2f62e14f8533bf
Tristan <tristan.cendan@gmail.com>
2024-10-09 00:30:38 +0200

    feat(firebase): created signUp screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Tabs } from 'expo-router';
 import React from 'react';
 
@@ -41,3 +42,44 @@ export default function TabLayout() {
     </Tabs>
   );
 }
+||||||| parent of e039983 (feat(firebase): created signUp screen)
+=======
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
+      
+    </Tabs>
+  );
+}
+>>>>>>> e039983 (feat(firebase): created signUp screen)
