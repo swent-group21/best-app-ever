4d769551f0b49c48569ff5afb2e74ec01cb3c45b
Tristan <tristan.cendan@gmail.com>
2024-10-09 13:03:42 +0200

    refactor(homepage): rolled back to default home
    This was modified for testing purposes, rolling back to default screen.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,46 +1,11 @@
-import { GoogleAuthProvider } from "../../firebase/Firebase";
-// import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "../../firebase/Firebase";
-import { View, Text, Button, Platform, StyleSheet } from 'react-native';
-import GoogleAuthConfig from "../../firebase/GoogleAuthConfig";
-import * as Google from "expo-auth-session/providers/google";
-import { useEffect } from "react";
+import { View, Text } from 'react-native';
 
-export default function SignUpScreen() { 
-    const config = Platform.select({
-    web: GoogleAuthConfig.web,
-    ios: GoogleAuthConfig.ios,
-    android: GoogleAuthConfig.android,
-  });
-
-  const [request, response, promptAsync] = Google.useAuthRequest(config);
-
-  useEffect(() => {
-    if (response?.type === "success") {
-      const { id_token } = response.params;
-      const credential = GoogleAuthProvider.credential(id_token);
-    }
-  }, [response]);
-
-  return ( 
-    <View style={styles.centered}> 
-      <Text style={styles.title}>Sign up</Text> 
-      <Button title="Sign up with Google" onPress={() => promptAsync()} />
-    </View> 
-  ); 
-} 
-  
-const styles = StyleSheet.create({ 
-  centered: { 
-    flex: 1, 
-    justifyContent: "center", 
-    alignItems: "center", 
-  }, 
-  title: { 
-    fontSize: 18, 
-    marginVertical: 2, 
-  }, 
-  subtitle: { 
-    fontSize: 14, 
-    color: "#888", 
-  }, 
-});
+export default function HomeScreen() {
+  return (
+    <View className="bg-cover bg-sky-500 ">
+      <Text className="text-3xl"> 
+        HELLO THIS IS THE HOME SCREEN
+      </Text>
+    </View>
+  );
+}
