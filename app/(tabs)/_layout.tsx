12926e74c8d5d6b594f5453e33b483fa923c13b0
Margaux Dausque <margauxdausque@tsf-508-wpa-2-026.epfl.ch>
2024-10-15 18:39:51 +0200

    fix(ui): styled interface sign up

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,15 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 
 export default function TabLayout() {
+<<<<<<< HEAD
 
+||||||| parent of dc43462 (fix(ui): styled interface sign up)
+  const colorScheme = useColorScheme();
+
+=======
+  const colorScheme = useColorScheme();
+// TODO: INSTEAD OF TOASTS, USE THE NAVIGATION STACK 
+>>>>>>> dc43462 (fix(ui): styled interface sign up)
   return (
     <Tabs
       screenOptions={{
--- app/auth/SignUp.tsx
+++ app/auth/SignUp.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, StyleSheet, Image, Platform } from 'react-native';
 import { TextInput } from 'react-native'
 import { Button } from 'react-native'
+import {SafeAreaView} from 'react-native';
 
 export default function SignUp() {
     const [name, setName] = React.useState("");
@@ -11,33 +12,41 @@ export default function SignUp() {
     const [confirmPassword, setConfirmPassword] = React.useState("");
     return (
         <View>
-        <Text>Tell us about you !</Text>
-        <TextInput
+        <View style = {styles.box}> </View>
+        <Text style = {styles.title}>Tell us about you !</Text>
+        <TextInput 
+          style = {styles.input}
           placeholder='Name'
           onChangeText={(text) => setName(text)}
+
         />
 
         <TextInput
+          style = {styles.input}
           placeholder='Surname'
           onChangeText={(text) => setSurname(text)}
+
         />
 
         <TextInput
+          style = {styles.input}
           placeholder= 'Email'
           onChangeText={(text) => setEmail(text)}
         />
 
         <TextInput
+          style = {styles.input}
           placeholder='Password'
           onChangeText={(text) => setPassword(text)}
         />
 
         <TextInput
+          style = {styles.input}
           placeholder= 'Confirm Password'
           onChangeText={(text) => setConfirmPassword(text)} 
           />
 
-        <Button title="Strive with us" onPress={() => alert("Sign Up")} />
+        <Button title="Strive with us" onPress={() => alert("Sign Up")} color = '#2C2C2C' />
         
         <Text> 
             OR
@@ -49,3 +58,52 @@ export default function SignUp() {
 
     );
     }
+
+    const styles = StyleSheet.create({
+        input: {
+          backgroundColor: '#fff',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding : 20,
+          margin: 10,
+          minWidth: 240,
+          overflow: 'hidden',
+          borderRadius: 10,
+          position: 'relative',
+        },
+        title: {
+          fontFamily: 'Inter-Bold',
+          fontSize: 40,
+          fontWeight: 700,
+          left:0,
+          letterSpacing: 0.15,
+          lineHeight: 57,
+          position: 'relative',
+          textAlign: 'right',
+          padding: 20,
+          
+      },
+
+      buttonStrive: {
+        alignItems: 'center',
+        display: 'flex',
+        gap : 10,
+        position: 'relative',
+        color: '#1E1E1E',
+      
+    },
+  
+    box : {
+      height : 629, 
+      width : 544,
+      color: '##e6bc95',
+      left : 0,
+      position: 'static',
+      top: 0,
+      borderRadius: 244,
+    }
+  
+  });
+
+
+
