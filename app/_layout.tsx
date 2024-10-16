21199c1d5badde59c6737344009e8e4bc203f101
Margaux Dausque <margauxdausque@tsf-508-wpa-1-059.epfl.ch>
2024-10-16 16:58:51 +0200

    style(signup): adjusted some code in styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
 
+
 export default function RootLayout() {
   return (
     <>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: true }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </>
   );
