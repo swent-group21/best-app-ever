5bce6f74633e4563b8df6a3c30e0d661961a7d9a
Margaux Dausque <margauxdausque@tsf-508-wpa-2-086.epfl.ch>
2024-10-16 21:25:14 +0200

    feat(ui): improved sign in screen design Integrated icon into Google and Facebook buttons Corrected some bugs in UI Email is now proposed when trying to write in the input text

--- app/(tabs)/SignUp.tsx
+++ app/(tabs)/SignUp.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image, Platform, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, Image, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native'
 import { Button } from 'react-native'
-import {SafeAreaView} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
 
 export default function SignUp() {
     const [name, setName] = React.useState("");
@@ -11,143 +13,167 @@ export default function SignUp() {
     const [password, setPassword] = React.useState("");
     const [confirmPassword, setConfirmPassword] = React.useState("");
     return (
+      
       <ScrollView>
        <View style = {styles.backround}>
+          {/* Color of the backround */}
 
+        {/* The backround image */} 
         <Image
           source={require('@/assets/images/sign-up-screen/Ellipse 3.png')}
           style={styles.backroundimage}
-          
-        
         />
        
+        {/* Title of the screen */}
         <Text style = {styles.title}>Tell us about you !</Text>
-        <Text style = {styles.titleinput}> Name</Text>
-        <TextInput 
-          style = {styles.input}
-          placeholder='Name'
-          onChangeText={(text) => setName(text)}
 
-        />
+        {/* The input fields */}
+        <View style = {styles.inputColumn}>
+            
+            <Text style = {styles.titleinput}>Name</Text>
+            <TextInput 
+              style = {styles.input}
+              placeholder='Name'
+              placeholderTextColor="#888"
+              onChangeText={(text) => setName(text)}
+
+            />
+            
+            <Text style = {styles.titleinput}>Surname</Text>
+            <TextInput
+              style = {styles.input}
+              placeholder='Surname'
+              placeholderTextColor="#888"
+              onChangeText={(text) => setSurname(text)}
+
+            />
+
+            <Text style = {styles.titleinput}>Email</Text>
+            <TextInput
+              style = {styles.input}
+              placeholder="example@your.domain"
+              placeholderTextColor="#888"
+              autoComplete='email'
+              inputMode='email'
+              keyboardType='email-address'
+              autoCapitalize='none'
+              onChangeText={(text) => setEmail(text)}
+            />
+
+            <Text style = {styles.titleinput}>Password</Text>
+            <TextInput
+              style = {styles.input}
+              placeholder='Password'
+              placeholderTextColor="#888"
+              onChangeText={(text) => setPassword(text)}
+            />
+
+            <Text style = {styles.titleinput}>Confirm Password</Text>
+            <TextInput
+              style = {styles.input}
+              placeholder= 'Confirm Password'
+              placeholderTextColor="#888"
+              onChangeText={(text) => setConfirmPassword(text)} 
+              />
+
+<TouchableOpacity style={styles.buttonStrive} onPress={() => alert('Sign Up')}>
+            <Text style={styles.buttonText}>Strive with us</Text>
+        </TouchableOpacity>
+
         
-        <Text style = {styles.titleinput}> Surname</Text>
-        <TextInput
-          style = {styles.input}
-          placeholder='Surname'
-          onChangeText={(text) => setSurname(text)}
-
-        />
-
-        <Text style = {styles.titleinput}> Email</Text>
-        <TextInput
-          style = {styles.input}
-          placeholder= 'Email'
-          onChangeText={(text) => setEmail(text)}
-        />
-
-        <Text style = {styles.titleinput}> Password</Text>
-        <TextInput
-          style = {styles.input}
-          placeholder='Password'
-          onChangeText={(text) => setPassword(text)}
-        />
-
-        <Text style = {styles.titleinput}> Confirm Password</Text>
-        <TextInput
-          style = {styles.input}
-          placeholder= 'Confirm Password'
-          onChangeText={(text) => setConfirmPassword(text)} 
-          />
-
-        <Button title="Strive with us" onPress={() => alert("Sign Up")} color = '#2C2C2C' />
-        
+        {/* OR */}
         <Text style = {styles.or}> 
             OR
         </Text>
 
-        <Button title="Sign Up with Google" onPress={() => alert("Sign Up with Google")} />
-        <Button title="Sign Up with Facebook" onPress={() => alert("Sign Up with Facebook")} />
+        {/* Sign Up buttons for Google and Facebook */}
+        <TouchableOpacity style={styles.buttonContinueWith} onPress={() => alert('Sign In with Google')}>
+        <View style={styles.buttonIcon}>
+            <Image source={require('@/assets/images/sign-up-screen/google.png')} style={styles.icon} />
+            <Text style={styles.buttonText}>Continue with Google</Text>
+        </View>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity style={styles.buttonContinueWith} onPress={() => alert('Sign In with Facebook')}>
+        <View style={styles.buttonIcon}>
+            <Image source={require('@/assets/images/sign-up-screen/facebook.png')} style={styles.icon} />
+            <Text style={styles.buttonText}>Continue with Facebook</Text>
+        </View>
+        </TouchableOpacity>
+        
+
+        </View>
+        {/* Register Buttons */}
+
+        
+        
         </View>
 
-        </ScrollView> 
+        </ScrollView>
+
+      
     );
    
     }
 
     const styles = StyleSheet.create({
         input: {
-          backgroundColor: '#fff',
-          alignItems: 'center',
-          justifyContent: 'center',
-          padding : 20,
-          margin:10,
-          minWidth: 240,
-          overflow: 'hidden',
-          borderRadius: 10,
-          position: 'relative',
-          borderColor: '#d9d9d9',
-          borderWidth: 0.5, 
+          width: '100%',
+          height: height * 0.06,  
+          borderWidth: 1,
+          borderRadius: 15,
+          borderColor: '#ccc',
+          paddingLeft: 20,
+          marginBottom: height * 0.02,
           
-          left: 0
           
           
         },
         title: {
-          fontFamily: 'Title Page',
-          fontSize: 38,
+          fontSize: width * 0.14,  
+          color: 'black',
           fontWeight: 'bold',
-          left:0,
-          top: 30,
-          letterSpacing: 0.15,
-          lineHeight: 57,
-          position: 'relative',
-          textAlign: 'center',
-          padding: 20,
+          textAlign: 'right',
+          paddingTop: height * 0.12,
+          paddingBottom: height * 0.05,
           
       },
         buttonStrive: {
-        alignItems: 'center',
-        display: 'flex',
-        gap : 10,
-        position: 'relative',
-        color: '#1E1E1E',
-        borderRadius : 10,
-        backgroundColor: '#E6BC95',
-        width: 200,
-        minWidth: 240,
-        borderEndEndRadius : 10,
-        borderEndStartRadius : 10,
-        borderWidth: 0.5,
-        borderColor: '#d9d9d9',
+          width: '100%',
+          height: height * 0.05,  
+          backgroundColor: '#E6BC95',
+          borderRadius: 15,
+          justifyContent: 'center',
+          alignItems: 'center',
       
     },
-  
-        box : {
-        height : 629, 
-        width : 544,
-        color: '##e6bc95',
-        left : 0,
-        position: 'static',
-        top: 0,
-        borderRadius: 244,
-     }, 
 
         or : {
         position : 'relative',
         textAlign: 'center',
-        fontFamily: 'Inter-Bold',
-        lineHeight: 10,
-        fontSize: 10, 
+        height : height * 0.04,
+        fontSize: width * 0.03, 
         textAlignVertical: 'center',
+        color: 'black',
+        paddingTop: height * 0.02,
+
+        
      }, 
 
      backround : {
-      backgroundColor : '#FFFFFF',
+      backgroundColor : 'white',
+      flex : 1,
+      alignItems : 'center',
+      justifyContent : 'flex-start',
      }, 
 
      titleinput : {
-
-      left: 20,
+      fontSize: width * 0.04,  
+      color: 'black',
+      width: '100%',
+      textAlign: 'left',
+      marginBottom: height * 0.01,
 
      }, 
 
@@ -155,7 +181,43 @@ export default function SignUp() {
       position : 'absolute',
       top : 0,
       left : 0,
-     }
+     }, 
+
+     buttonText: {
+      fontSize: width * 0.045,  
+      color: 'black',
+  },
+
+  inputColumn: {
+    width: '83%',
+    height: '60%',
+    flexDirection: 'column',       
+    justifyContent: 'center', 
+    alignItems: 'center',       
+    gap: height * 0.001,
+},
+
+buttonContinueWith: {
+  width: '100%',
+  height: height * 0.05,  
+  backgroundColor: '#F5F5F5',
+  borderRadius: 15,
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginBottom: height * 0.02,
+},
+
+buttonIcon: {
+  flexDirection: 'row', 
+  alignItems: 'center', 
+},
+
+icon: {
+  width: 20, 
+  height: 20, 
+  marginRight: 10, 
+},
+
   
   });
 
