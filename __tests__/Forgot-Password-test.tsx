cc81c198cda4ee86ad663e23c3319feb86f63daf
Roman <roman.batut@epfl.ch>
2024-10-18 00:54:05 +0200

    feat(tests): added basic forgotPasswordScreen tests

---
+++ __tests__/Forgot-Password-test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import ForgotPasswordScreen from '../app/auth/forgot-password';
+
+describe('ForgotPasswordScreen Component', () => {
+  it('renders the "Forgot your Password?" title', () => {
+    TestTextAppearsOnScreen('Forgot your Password ?');
+  });
+
+  it('renders the email input', () => {
+    TestTextAppearsOnScreen('Email');
+    TestButtonAppearsOnScreen('emailInput');
+  });
+
+  it('renders the Cancel button', () => {
+    TestButtonAppearsOnScreen('cancelButton');
+  });
+
+  it('renders the Reset Password button', () => {
+    TestButtonAppearsOnScreen('resetPasswordButton');
+  });
+});
+
+function TestTextAppearsOnScreen(text: string) {
+    const { getByText } = render(<ForgotPasswordScreen />);
+        
+    expect(getByText(text)).toBeTruthy(); 
+}
+
+function TestButtonAppearsOnScreen(text: string) {
+    const { getByTestId } = render(<ForgotPasswordScreen />);
+        
+    expect(getByTestId(text)).toBeTruthy(); 
+}
