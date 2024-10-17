5e46c6a76037c1096f266795ba94703b1868d392
Tristan <tristan.cendan@gmail.com>
2024-10-17 21:29:40 +0200

    fix(tests): modularized to reduce repetition

--- __tests__/WelcomeScreens-test.tsx
+++ __tests__/WelcomeScreens-test.tsx
@@ -3,27 +3,25 @@ import { render } from '@testing-library/react-native';
 import WelcomeScreens from '../app/(tabs)/welcome_screens'; 
 
 describe('WelcomeScreens Component', () => {
-    test('welcome_intro correctly displays title', () => {
-        const { getByText } = render(<WelcomeScreens />);
-        
-        expect(getByText('So what is Strive about ?')).toBeTruthy(); 
+    it('renders the intro screen', () => {
+        TestTextAppearsOnScreen('So what is\nStrive\nabout ?');
     });
 
-    test('welcome_concept correctly displays title', () => {
-        const { getByText } = render(<WelcomeScreens />);
-        
-        expect(getByText('Compete with your friends and people around you\nBecome the goat and win prizes!')).toBeTruthy(); 
+    it('renders the concept screen', () => {
+        TestTextAppearsOnScreen('Compete with your friends and people around you\nBecome the goat and win prizes!');
     });
 
-    test('welcome_personal correctly displays title', () => {
-        const { getByText } = render(<WelcomeScreens />);
-        
-        expect(getByText('Become the best version of yourself\nInteract with motivated people to reach your goals !')).toBeTruthy(); 
+    it('renders the personal screen', () => {
+        TestTextAppearsOnScreen('Become the best version of yourself\nInteract with motivated people to reach your goals !');
     });
 
-    test('welcome_final correctly displays title', () => {
-        const { getByText } = render(<WelcomeScreens />);
-        
-        expect(getByText('Ready to\nStrive?')).toBeTruthy(); 
+    it('renders the final screen', () => {
+        TestTextAppearsOnScreen('Ready to\nStrive?');
     });
 });
+
+function TestTextAppearsOnScreen(text: string) {
+    const { getByText } = render(<WelcomeScreens />);
+        
+    expect(getByText(text)).toBeTruthy(); 
+}
