9e5cf6b7f24dbcfce9aec5b3886db543c8821e33
Tristan <tristan.cendan@gmail.com>
2024-11-01 04:23:55 +0100

    refactor: used tailwind to style

--- app/screens/welcome/final_screen.tsx
+++ app/screens/welcome/final_screen.tsx
@@ -15,24 +15,26 @@ export default function WelcomeConceptScreen() {
   const router = useRouter();
   return (
     <View className="h-screen w-screen">
-      <View style={styles.ovalShapeOne} />
-      <View style={styles.ovalShapeTwo} />
-      <Text style={styles.title}>Ready to{"\n"}Strive?</Text>
+      <View style={shape.ovalOne} />
+      <View style={shape.ovalTwo} />
+      <Text className="text-7xl font-black pl-3 pt-72">
+        Ready to{"\n"}Strive?
+      </Text>
 
-      <View style={styles.buttonContainer}>
+      <View className="items-center pt-14">
         <TouchableOpacity
-          style={styles.buttonAccount}
+          className="w-80 h-11 bg-black rounded-lg justify-center items-center"
           onPress={() => router.push("/screens/auth/sign_in_screen")}
         >
-          <Text style={styles.buttonText}>Login</Text>
+          <Text className="color-white font-semibold">Login</Text>
         </TouchableOpacity>
         {/* Add some space between the buttons */}
         <Text />
         <TouchableOpacity
-          style={styles.buttonAccount}
+          className="w-80 h-11 bg-black rounded-lg justify-center items-center"
           onPress={() => router.push("/screens/auth/sign_up_screen")}
         >
-          <Text style={styles.buttonText}>Sign Up</Text>
+          <Text className="color-white font-semibold">Sign Up</Text>
         </TouchableOpacity>
         {/* Add some space between the buttons */}
         <Text />
@@ -49,8 +51,8 @@ export default function WelcomeConceptScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  ovalShapeOne: {
+const shape = StyleSheet.create({
+  ovalOne: {
     position: "absolute",
     top: SCREEN_HEIGHT * 0.8,
     left: -SCREEN_WIDTH * 0.3,
@@ -59,7 +61,7 @@ const styles = StyleSheet.create({
     borderRadius: SCREEN_WIDTH * 0.7,
     backgroundColor: "#E6BC95",
   },
-  ovalShapeTwo: {
+  ovalTwo: {
     position: "absolute",
     top: -SCREEN_HEIGHT * 0.4,
     left: SCREEN_WIDTH * 0.3,
@@ -67,31 +69,5 @@ const styles = StyleSheet.create({
     height: SCREEN_HEIGHT * 0.7,
     borderRadius: SCREEN_WIDTH * 0.7,
     backgroundColor: "#E6BC95",
-  },
-  title: {
-    paddingTop: SCREEN_HEIGHT * 0.3,
-    paddingLeft: SCREEN_WIDTH * 0.05,
-    fontSize: 64,
-    fontWeight: "900",
-    color: "#000",
-    lineHeight: 62,
-    paddingBottom: SCREEN_HEIGHT * 0.12,
-  },
-  buttonContainer: {
-    flex: 1,
-    alignItems: "center",
-    paddingBottom: 60,
-  },
-  buttonAccount: {
-    width: "80%",
-    height: SCREEN_HEIGHT * 0.05,
-    backgroundColor: "#000",
-    borderRadius: 15,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  buttonText: {
-    fontWeight: "600",
-    color: "#FFF",
   },
 });
