76eab04c6cf082c365d0c942cbfc75327d2a167b
Tristan <tristan.cendan@gmail.com>
2024-11-08 14:39:18 +0100

    fix: removed loginWithGoogle method

--- types/Auth.tsx
+++ types/Auth.tsx
@@ -1,62 +1,20 @@
 import {
   auth,
   createUserWithEmailAndPassword,
-  signInWithCredential,
   signInWithEmailAndPassword,
 } from "@/firebase/Firebase";
 import FirestoreCtrl, { DBUser } from "@/firebase/FirestoreCtrl";
 
-/*** 
+/***
  * Function to check if the email is valid
  * @param email - email to be checked
  * @returns - true if the email is valid, false otherwise
  */
-export function isValidEmail(email : string) {
-  let reg = /^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
+export function isValidEmail(email: string) {
+  let reg =
+    /^[a-zA-Z0-9]+([._-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
   return reg.test(email);
 }
-
-export const logInWithGoogle = (
-  credential: any,
-  router: any,
-  firestoreCtrl: FirestoreCtrl
-) => {
-  signInWithCredential(auth, credential).then((result) => {
-    const newUser =
-      result.user.metadata.creationTime === result.user.metadata.lastSignInTime;
-    if (newUser) {
-      const userData: DBUser = {
-        name: result.user.displayName || "",
-        email: result.user.email || "",
-        createdAt: new Date(),
-      };
-
-      firestoreCtrl.createUser(result.user.uid, userData).then(() => {
-        router.navigate("@app/home/PLACEHOLd_home_screen");
-      });
-    } else {
-      firestoreCtrl
-        .getUser(result.user.uid)
-        .then((user: any) => {
-          if ( user ) {
-            router.navigate("@app/home/PLACEHOLd_home_screen");
-          }
-        })
-        .catch(() => {
-          // User might not exist in the database
-          firestoreCtrl
-            .createUser(result.user.uid, {
-              name: result.user.displayName || "",
-              email: result.user.email || "",
-              createdAt: new Date(),
-            })
-            .then(() => {
-              router.navigate("@app/home/PLACEHOLd_home_screen");
-            });
-        });
-    }
-  });
-};
 
 export const logInWithEmail = async (
   email: string,
@@ -82,7 +40,7 @@ export const logInWithEmail = async (
                 router.navigate("../home/home_screen");
               });
           });
-        if ( user ) {
+        if (user) {
           router.navigate("../home/home_screen");
         }
       } else {
@@ -117,7 +75,10 @@ export const signUpWithEmail = async (
             router.navigate("../home/home_screen");
           })
           .catch((error) => {
-            console.log("FirestoreCtrl failed to create user due to following error \n", error);
+            console.log(
+              "FirestoreCtrl failed to create user due to following error \n",
+              error,
+            );
           });
       })
       .catch((error) => {
@@ -127,4 +88,3 @@ export const signUpWithEmail = async (
     console.log("Please input email and password.");
   }
 };
-
