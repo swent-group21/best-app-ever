4f06b606344ec9d99fc71ceb5969c4ac42559f26
Tristan-C789 <tristan.cendan@gmail.com>
2024-11-20 22:59:29 +0100

    feat(map): added permission handler

--- app/screens/map/map_screen.tsx
+++ app/screens/map/map_screen.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import MapView from "react-native-maps";
+import {
+  useForegroundPermissions,
+  getCurrentPositionAsync,
+} from "expo-location";
 
 const MapScreen = () => {
+  const [permission, requestPermission] = useForegroundPermissions();
+  const [location, setLocation] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+  });
+
+  useEffect(() => {
+    async function fetchLocation() {
+      await requestPermission();
+      if (permission && permission.status === "granted") {
+        const location = await getCurrentPositionAsync();
+        setLocation(location.coords);
+      }
+    }
+    fetchLocation();
+  }, [permission]);
+
+  if (!location) {
+    return <View style={styles.container} />;
+  }
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} testID="mapView" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          ...location,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        testID="mapView"
+      />
     </View>
   );
 };
