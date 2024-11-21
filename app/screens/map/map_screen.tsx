67abc3cbb5d79be9c6f372456f4f6ce166961bd9
Tristan-C789 <tristan.cendan@gmail.com>
2024-11-21 16:56:47 +0100

    fix(map): location permission correctly handled

--- app/screens/map/map_screen.tsx
+++ app/screens/map/map_screen.tsx
@@ -2,40 +2,51 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 import {
-  useForegroundPermissions,
+  requestForegroundPermissionsAsync,
   getCurrentPositionAsync,
+  LocationObject,
 } from "expo-location";
 
-const MapScreen = () => {
-  const [permission, requestPermission] = useForegroundPermissions();
-  const [location, setLocation] = useState({
+const defaultLocation = {
+  coords: {
     latitude: 37.78825,
     longitude: -122.4324,
-  });
+  },
+} as LocationObject;
+
+/**
+ * The MapScreen component displays a map centered on the user's current location, if available.
+ *
+ * @returns The MapScreen component
+ */
+const MapScreen = () => {
+  const [location, setLocation] = useState<LocationObject>(defaultLocation);
 
   useEffect(() => {
-    async function fetchLocation() {
-      await requestPermission();
-      if (permission && permission.status === "granted") {
-        const location = await getCurrentPositionAsync();
-        setLocation(location.coords);
+    /**
+     * Asks for permission to access the user's location and sets the location state to the user's current location.
+     */
+    async function getCurrentLocation() {
+      let { status } = await requestForegroundPermissionsAsync();
+
+      if (status === "granted") {
+        let location = await getCurrentPositionAsync();
+        setLocation(location);
       }
     }
-    fetchLocation();
-  }, [permission]);
 
-  if (!location) {
-    return <View style={styles.container} />;
-  }
+    getCurrentLocation();
+  }, []);
 
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
         initialRegion={{
-          ...location,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0,
+          longitudeDelta: 0,
         }}
         testID="mapView"
       />
