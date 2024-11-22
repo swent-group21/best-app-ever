02ba28aae50c59e6af9e469a7fe3c92772db69c3
Tristan-C789 <tristan.cendan@gmail.com>
2024-11-22 09:05:05 +0100

    tests: added more tests (WIP)

--- __tests__/MapScreen-test.tsx
+++ __tests__/MapScreen-test.tsx
@@ -32,4 +32,47 @@ describe("MapScreen Component", () => {
 
     expect(Location.requestForegroundPermissionsAsync).toHaveBeenCalled();
   });
+
+  it("gets the current location", () => {
+    render(<MapScreen />);
+
+    expect(Location.getCurrentPositionAsync).toHaveBeenCalled();
+  });
+
+  it("displays the default location if permission is not granted", () => {
+    (Location.requestForegroundPermissionsAsync as jest.Mock).mockResolvedValue(
+      {
+        status: "denied",
+      },
+    );
+
+    const { getByTestId } = render(<MapScreen />);
+
+    expect(getByTestId("mapView").props).toHaveProperty("initialRegion", {
+      latitude: 43.6763,
+      longitude: 7.0122,
+    });
+  });
+
+  it("displays markers for challenges with locations", () => {
+    const challenges = [
+      {
+        location: {
+          latitude: 37.7749,
+          longitude: -122.4194,
+        },
+      },
+    ];
+
+    (Location.getCurrentPositionAsync as jest.Mock).mockResolvedValue({
+      coords: {
+        latitude: 37.7749,
+        longitude: -122.4194,
+      },
+    });
+
+    const { getByTestId } = render(<MapScreen />);
+
+    expect(getByTestId("mapView").props).toHaveProperty("markers", challenges);
+  });
 });
