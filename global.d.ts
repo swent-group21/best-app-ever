97d4ccc428f0a4171258949106184f8dd82817b6
Margaux Dausque <margauxdausque@Margauxs-MacBook-Air-2.local>
2024-12-06 09:46:54 +0100

    fix: add module for Icon

---
+++ global.d.ts
@@ -0,0 +1,6 @@
+declare module "react-native-vector-icons/MaterialIcons" {
+    import { IconProps } from "react-native-vector-icons/Icon";
+    const Icon: React.ComponentType<IconProps>;
+    export default Icon;
+  }
+  
